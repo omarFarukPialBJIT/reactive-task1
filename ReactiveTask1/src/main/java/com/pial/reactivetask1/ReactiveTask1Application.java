8bd8c67cd5f3cfcbfa4f54d09ad7d79ac30d4d6e
omarFarukPialBJIT <omarfarukpial1@gmail.com>
2023-09-14 16:49:24 +0600

    init

---
+++ ReactiveTask1/src/main/java/com/pial/reactivetask1/ReactiveTask1Application.java
@@ -0,0 +1,13 @@
+package com.pial.reactivetask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveTask1Application.class, args);
+	}
+
+}
---
+++ ReactiveTask1/src/test/java/com/pial/reactivetask1/ReactiveTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pial.reactivetask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
